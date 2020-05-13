3afb93fbd42bb28102ad9ace96a351b2a7565aa2
kishan pandey <41380677+kishanrocks7@users.noreply.github.com>
2020-05-13 17:47:16 +0530

    -

--- app.js
+++ app.js
@@ -1,11 +1,89 @@
 const dialogflow = require('dialogflow');
 const uuid = require('uuid');
+const http = require('http');
 const express=require('express');
+const socketio = require('socket.io');
 const bodyParser=require('body-parser');
 const port=8081;
 const path = require('path');
 const app = express();
 const sessionId = uuid.v4();
+
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+const botName = 'ChatCord Bot';
+
+
+
+
+
+
+
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  });
+});
+
+
+
+
+
+
+
 
 app.use(bodyParser.urlencoded({
     extended:false
